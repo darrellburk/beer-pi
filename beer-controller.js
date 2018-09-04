0b6e86014c136d28c5e4ea9bbe96d20e5d54fe01
dburk <darrell.burk@gmail.com>
2018-09-03 22:37:21 -0500

    WIP

--- beer-controller.js
+++ beer-controller.js
@@ -1,5 +1,5 @@
 const Gpio = require('onoff').Gpio;
-const power = new Gpio(17, 'out');
+var power = new Gpio(17, 'out');
 const sensor = require('ds18b20-raspi');
 
 var value = 0;
@@ -8,6 +8,16 @@ var value = 0;
   { id: '28-041652951fff', t: 75.2 } ]
 
 */
+
+/*
+Call onExit when the app is terminating because process.exit() is called or there is no more work to do.
+*/
+process.on("exit", onExit);
+process.on("SIGINT", function() {
+  console.log("Received SIGINT");
+  process.exit();
+});
+
 
 var probes = discoverTemperatureProbes();
 
@@ -39,4 +49,27 @@ function discoverTemperatureProbes() {
 
 function readProbeCallback(error, readings) {
   console.log("readProbeCallback: readings="+readings+", error="+error);
+}
+
+/**
+ * Handler that is intended to be called upon receipt of any external signal that requests the
+ * app to terminate.
+ */
+function exitRequested() {
+  process.exit();
+}
+
+/**
+ * Handler that is invoked when the app is exiting (intended to be in ALL normal and abnormal
+ * exit cases) and should do any needed cleanup like TURNING THE FREEZER OFF!!!
+ * 
+ * @param {} code 
+ */
+function onExit(code) {
+  // turn off the power and then unexport that GPIO pin
+  power.writeSync(0);
+  power.unexport();
+  power = null;
+
+  console.log("beer-controller is exiting with code "+code);
 }
